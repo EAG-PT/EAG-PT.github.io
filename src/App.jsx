ffe904ba9cf6db18159672fabb51e3fc2425cac8
YangXijie <564197835@qq.com>
2026-04-28 12:33:30 +0800

    add anthors

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,61 @@ function App() {
   return (
     <>
       <h1>
-        EAG-PT: Emission-Aware Gaussians and Path Tracing for Diffuse Indoor
-        Scene Reconstruction and Editing
+        EAG-PT: Emission-Aware Gaussians and Path Tracing <br />
       </h1>
-      <h2>WIP</h2>
+      <h2>for Diffuse Indoor Scene Reconstruction and Editing</h2>
+      <h3>(SIGGRAPH 2026 Conference Paper)</h3>
+
+      <p>
+        <a href="https://xijie-yang.github.io" title="杨希杰">
+          Xijie Yang
+        </a>
+        <sup>1,2</sup>{" "}
+        <a href="https://mulinyu.github.io/" title="于沐霖">
+          Mulin Yu
+        </a>
+        <sup>2*</sup>{" "}
+        <a
+          href="https://scholar.google.com/citations?user=V4miywEAAAAJ"
+          title="蒋昌健"
+        >
+          Changjian Jiang
+        </a>
+        <sup>1</sup>{" "}
+        <a href="https://cskrren.github.io/" title="任柯睿">
+          Kerui Ren
+        </a>
+        <sup>3,2</sup>{" "}
+        <a href="https://inspirelt.github.io/" title="鲁涛">
+          Tao Lu
+        </a>
+        <sup>2</sup>{" "}
+        <a href="https://oceanpang.github.io/" title="庞江淼">
+          Jiangmiao Pang
+        </a>
+        <sup>2</sup>{" "}
+        <a href="https://www.ie.cuhk.edu.hk/faculty/lin-dahua/" title="林达华">
+          Dahua Lin
+        </a>
+        <sup>4,2</sup>{" "}
+        <a href="https://datascience.hku.hk/people/bo-dai/" title="戴勃">
+          Bo Dai
+        </a>
+        <sup>5,6*</sup>{" "}
+        <a href="https://eveneveno.github.io/lnxu/" title="徐霖宁">
+          Linning Xu
+        </a>
+        <sup>4,2</sup>
+      </p>
+
+      <p>
+        <sup>1</sup> Zhejiang University <sup>2</sup> Shanghai Artificial
+        Intelligence Laboratory <sup>*</sup> Corresponding authors.
+        <br />
+        <sup>3</sup> Shanghai Jiao Tong University <sup>4</sup> The Chinese
+        University of Hong Kong <sup>5</sup> The University of Hong Kong{" "}
+        <sup>6</sup> Feeling AI
+      </p>
     </>
   );
 }
